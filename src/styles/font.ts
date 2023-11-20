695e719f9c4d2a546ca30146f5f22705af651bd0
AbdelrahmanElhosiny <abdohosini13@gmail.com>
2023-11-20 22:07:38 +0200

    add `fonts`

--- src/styles/font.ts
+++ src/styles/font.ts
@@ -1,8 +1,29 @@
 import { css } from "@emotion/react";
 
 const font = {
-  thin: css`
-    font-family: "placholder";
+  Regular: css`
+    font-family: "Jet-Regular";
+  `,
+  Bold: css`
+    font-family: "Jet-Bold";
+  `,
+  Light: css`
+    font-family: "Jet-Light";
+  `,
+  Thin: css`
+    font-family: "Jet-Thin";
+  `,
+  ItalicRegular: css`
+    font-family: "Jet-Italic-Regular";
+  `,
+  ItalicBold: css`
+    font-family: "Jet-Italic-Bold";
+  `,
+  ItalicLight: css`
+    font-family: "Jet-Italic-Light";
+  `,
+  ItalicThin: css`
+    font-family: "Jet-Italic-Thin";
   `,
 };
 
