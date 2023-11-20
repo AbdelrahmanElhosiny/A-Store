ca0b8f31b80069cdda5cc14c0d1597ecaefd251c
AbdelrahmanElhosiny <abdohosini13@gmail.com>
2023-11-20 21:27:14 +0200

    add `color`

---
+++ src/styles/font.ts
@@ -0,0 +1,9 @@
+import { css } from "@emotion/react";
+
+const font = {
+  thin: css`
+    font-family: "placholder";
+  `,
+};
+
+export default font;
---
+++ src/styles/index.ts
@@ -0,0 +1,11 @@
+import font from "./font";
+import colorD from "./colorD";
+import colorL from "./colorL";
+
+const style = {
+  font,
+  colorD,
+  colorL,
+};
+
+export default style;
